bd51cff25a9a2782576491c5aceaba25d230e657
dllo <771448371@qq.com>
2017-12-12 16:19:38 +0800

    修改代码
    时间:2017年12月12日16:19:06

--- src/com/baidu/Hello.java
+++ src/com/baidu/Hello.java
@@ -14,6 +14,7 @@ public class Hello extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		// ssss
 		resp.setContentType("text/html");
 		PrintWriter printWriter= resp.getWriter();
 		printWriter.write("<h1>test</h1>");
